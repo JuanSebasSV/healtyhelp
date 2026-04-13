a98d2bb2d064c78f0cda60264684f73e62ed0124
JuanSebasSV <jssjuansebas11@gmail.com>
2026-04-13 15:01:15 -0500

    fix login google

--- server/server.js
+++ server/server.js
@@ -47,8 +47,17 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 app.use((req, res, next) => {
-  if (req.body) {
-    req.body = JSON.parse(filterXSS(JSON.stringify(req.body)));
+  if (req.body && typeof req.body === 'object') {
+    const sanitize = (obj) => {
+      for (const key in obj) {
+        if (typeof obj[key] === 'string') {
+          obj[key] = filterXSS(obj[key]);
+        } else if (typeof obj[key] === 'object' && obj[key] !== null) {
+          sanitize(obj[key]);
+        }
+      }
+    };
+    sanitize(req.body);
   }
   next();
 });
