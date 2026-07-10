e6caeabe0a1c8e6254eec22dc285e1530845d583
JuanSebasSV <jssjuansebas11@gmail.com>
2026-07-10 09:11:57 -0500

    fix 5

--- client/src/components/layout/FondoAnimado.jsx
+++ client/src/components/layout/FondoAnimado.jsx
@@ -1,7 +1,12 @@
 import { memo } from 'react';
 import './FondoAnimado.css';
 
-const FondoAnimado = memo(() => <div className="fondoAnimado" />);
+const FondoAnimado = memo(() => (
+  <>
+    <div className="fondoAnimado" aria-hidden="true" />
+    <div className="fondoTinte" aria-hidden="true" />
+  </>
+));
 FondoAnimado.displayName = 'FondoAnimado';
 
 export default FondoAnimado;
